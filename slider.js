f5afe54900cae9c76d16d03ac2b8222aa40860ba
Babyonmyway <94551388+babyobmyway@users.noreply.github.com>
2022-09-19 17:06:59 +0500

    Delete slider.js

--- slider.js
+++
@@ -1,6 +0,0 @@
-let offset = 0
-const sliderLine = document.querySelector('.masonry')
-document.querySelector('.pgn__next').addEventListener('click', function() {
-    offset += 360
-    sliderLine.style.left = -offset + 'px'
-})
